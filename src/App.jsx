6152f37a16bc2dc05bbe4e82808cc37206957004
Katarzyna Tchir <tchir.katarzyna@gmail.com>
2025-03-06 23:51:49 +0100

    done form validatio

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
 import { useState } from 'react';
+import { Container, H1 } from './styles/styles.js';
 import ApplicationForm from './components/ApplicationForm';
 import Modal from './components/Modal.jsx';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 20px;
-  margin: 5% 10%;
-`;
-
-const H1 = styled.h1`
-  font-size: 1.25rem;
-  font-weight: 600;
-  color: #1eab72;
-  text-align: center;
-`;
 
 const App = () => {
   const [userData, setUserData] = useState(null);
 
   return (
     <Container>
-      <H1>Formularz zgłoszeniowy na kurs programowania</H1>
       {userData === null ? (
-        <ApplicationForm setUserData={setUserData} />
+        <>
+          <H1>Formularz zgłoszeniowy na kurs programowania</H1>
+          <ApplicationForm setUserData={setUserData} />
+        </>
       ) : (
-        <Modal userData={userData} />
+        <>
+          <H1>Dane z formularza</H1>
+          <Modal userData={userData} />
+        </>
       )}
     </Container>
   );
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,6 +2,7 @@ const Modal = ({ userData }) => {
   return (
     <div>
       <p>Imię: {userData.firstName}</p>
+      <p>{JSON.stringify(userData, null, 2)}</p>
     </div>
   );
 };
